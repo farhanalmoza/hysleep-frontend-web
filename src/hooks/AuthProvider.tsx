4158b565222efe232b0f1517af7e1c9c182ef0e9
Farhan <oyisamfarhan@gmail.com>
2025-02-21 16:24:23 +0700

    set null default token

--- src/hooks/AuthProvider.tsx
+++ src/hooks/AuthProvider.tsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }: {children: any}) => {
-  const [token, setToken] = useState('s');
+  const [token, setToken] = useState(null);
 
   const login = (userToken: any) => {
     setToken(userToken);
